ad7e5cd3d3bf584c3600ff7aecc0334e221d2bfe
razeljoygammad-crypto <razeljoygammad@gmail.com>
2026-04-05 15:30:54 +0800

    Bot.py

--- Bot.py
+++ Bot.py
@@ -123,8 +123,6 @@ async def calc_command(interaction: discord.Interaction):
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
-
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
