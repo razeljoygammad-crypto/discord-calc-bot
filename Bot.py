c7b1e727f516a35343f3bc54e4eed25429879eff
razeljoygammad-crypto <razeljoygammad@gmail.com>
2026-04-09 01:35:47 +0800

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -22,7 +22,7 @@ def home():
     return "Bot is alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=1000)
 
 def keep_alive():
     t = Thread(target=run)
